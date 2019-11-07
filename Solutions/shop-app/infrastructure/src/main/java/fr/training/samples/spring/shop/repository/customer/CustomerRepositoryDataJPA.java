671ae95713cdfb91b171319bbe713b51cdda3385
springdoc <springdoc99@gmail.com>
2019-11-07 09:33:43 +0100

    project update

--- Solutions/shop-app/infrastructure/src/main/java/fr/training/samples/spring/shop/repository/customer/CustomerRepositoryDataJPA.java
+++
@@ -1,9 +0,0 @@
-package fr.training.samples.spring.shop.repository.customer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import fr.training.samples.spring.shop.domain.customer.CustomerEntity;
-
-public interface CustomerRepositoryDataJPA extends JpaRepository<CustomerEntity, String> {
-
-}
--- Solutions/shop-app/infrastructure/src/main/java/fr/training/samples/spring/shop/repository/item/ItemRepositoryDataJPA.java
+++
@@ -1,7 +0,0 @@
-package fr.training.samples.spring.shop.repository.item;
-
-import fr.training.samples.spring.shop.domain.item.ItemEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepositoryDataJPA extends JpaRepository<ItemEntity, String> {
-}
