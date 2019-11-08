30aea65643296a8c87b8cba2cbd5279ab590745d
springdoc <springdoc99@gmail.com>
2019-11-08 14:03:42 +0100

    project update

---
+++ Solutions/shop-app/shop-app-webmvc/src/test/java/com/training/sample/shopappwebmvc/ShopAppWebmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.sample.shopappwebmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAppWebmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
